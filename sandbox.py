992103b31ec102979a105d88383383a873f89ad5
Thomas Feuerborn <tfeuerbo@gmail.com>
2024-02-13 09:25:46 -0800

    added logging basic config for sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,5 @@
+import logging
+
 # import json
 
 # a = ["ts","uid","id.orig_h","id.orig_p","id.resp_h","id.resp_p","proto","service","duration","orig_bytes","resp_bytes","conn_state","local_orig","local_resp","missed_bytes","history","orig_pkts","orig_ip_bytes","resp_pkts","resp_ip_bytes","tunnel_parents","label","detailed-label"]
@@ -21,6 +23,8 @@ size_order = ['44','4','5','20','21','42','8','34','3','1','60','48','49','9','3
 from glob import glob
 
 directory = r"C:\Users\thomas.feuerborn\Documents\IoT Labeled Zeek Logs"
+
+logging.basicConfig(level='INFO')
 
 for i,prefix in enumerate(size_order[::-1]):
     print(f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")}) file {prefix}-1 ({i+1}/{len(size_order)})')
