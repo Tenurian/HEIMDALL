6f947cf0ca6002faff8a6faa5b1e5ed511d0a76d
Thomas Feuerborn <tfeuerbo@gmail.com>
2024-02-13 08:26:05 -0800

    Added infrastructure scripts

---
+++ logdb.py
@@ -0,0 +1,85 @@
+import os
+from datetime import datetime
+import sqlite3 as sql
+from sqlite3 import Error
+# from pathlib import Path
+
+from utils.ProgressBar import printProgressBar
+
+class LogDB:
+    __conn = None
+    __cursor = None
+
+    def __init__(self):
+        self.__conn = sql.connect(r'./.connlog.db')
+        self.__conn.row_factory = self.__dict_factory
+
+        try:
+            self.__cursor = self.__conn.cursor()
+        except Error as e:
+            print(f'Unable to connect to database. {e}')
+            exit(1)
+        pass
+
+    def setupDB(self):
+        self.__cursor.execute(
+                """
+CREATE TABLE IF NOT EXISTS conn_logs (
+    filename text NOT NULL,
+    ts real NOT NULL,
+    uid text PRIMARY KEY NOT NULL,
+    src_ip text NOT NULL,
+    src_port int NOT NULL,
+    dst_ip text NOT NULL,
+    dst_port int NOT NULL,
+    proto text NOT NULL,
+    service text,
+    duration real,
+    orig_bytes int,
+    resp_bytes int,
+    conn_state text,
+    local_orig text,
+    local_resp text,
+    missed_bytes int,
+    history text,
+    orig_pkts int,
+    orig_ip_bytes int,
+    resp_pkts int,
+    resp_ip_bytes int,
+    tunnel_parents blob,
+    label text,
+    detailed_label text
+)
+                """
+            )
+
+    def upsertLogfile(self, file):
+        print(f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")}) Reading {os.path.basename(file)} and updating database...')
+        sql_command = f'''
+INSERT INTO conn_logs(filename,ts,uid,src_ip,src_port,dst_ip,dst_port,proto,service,duration,orig_bytes,resp_bytes,conn_state,local_orig,local_resp,missed_bytes,history,orig_pkts,orig_ip_bytes,resp_pkts,resp_ip_bytes,tunnel_parents,label,detailed_label) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+    ON CONFLICT(uid) DO UPDATE SET filename = ?,ts = ?,src_ip = ?,src_port = ?,dst_ip = ?,dst_port = ?,proto = ?,service = ?,duration = ?,orig_bytes = ?,resp_bytes = ?,conn_state = ?,local_orig = ?,local_resp = ?,missed_bytes = ?,history = ?,orig_pkts = ?,orig_ip_bytes = ?,resp_pkts = ?,resp_ip_bytes = ?,tunnel_parents = ?,label = ?,detailed_label = ?
+'''
+        lines = 0
+        print(f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")})\t Counting lines in file')
+        with open(file, 'r') as logfile:
+            lines = len(logfile.readlines())-1
+        print(f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")})\t {lines} lines found.')
+        print(f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")})\t Parsing file & inserting data into database')
+        with open(file, 'r') as logfile:
+            for i,line in enumerate(logfile):
+                if not line.startswith('#'):
+                    f = lambda x: x if x != '-' else None
+                    fields = [f(field) for field in [os.path.basename(file).split('.')[0], *line.split()]]
+                    fields_no_uid = list(fields)
+                    fields_no_uid.remove(fields[2])
+                    self.__cursor.execute(sql_command, [*fields, *fields_no_uid])
+                    self.__conn.commit()
+                printProgressBar(iteration=i, total=lines, decimals=6)
+            pass
+        pass
+
+    def __dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
---
+++ sandbox.py
@@ -0,0 +1,29 @@
+# import json
+
+# a = ["ts","uid","id.orig_h","id.orig_p","id.resp_h","id.resp_p","proto","service","duration","orig_bytes","resp_bytes","conn_state","local_orig","local_resp","missed_bytes","history","orig_pkts","orig_ip_bytes","resp_pkts","resp_ip_bytes","tunnel_parents","label","detailed-label"]
+# b = ["time","string","addr","port","addr","port","enum","string","interval","count","count","string","bool","bool","count","string","count","count","count","count","set[string]","string","string"]
+# c = {}
+
+# for i,v, in enumerate(a):
+#     c[v] = b[i]
+
+# print(json.dumps(c, indent=2))
+
+from logdb import LogDB
+from datetime import datetime
+
+db = LogDB()
+db.setupDB()
+
+size_order = ['44','4','5','20','21','42','8','34','3','1','60','48','49','9','35','7','36','52','33','17','43','39']
+
+# import os
+from glob import glob
+
+directory = r"C:\Users\thomas.feuerborn\Documents\IoT Labeled Zeek Logs"
+
+for i,prefix in enumerate(size_order[::-1]):
+    print(f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")}) file {prefix}-1 ({i+1}/{len(size_order)})')
+    for file in glob(f'{directory}\\*-{prefix}-1.conn.log.labeled'):
+        db.upsertLogfile(file)
+        print()
